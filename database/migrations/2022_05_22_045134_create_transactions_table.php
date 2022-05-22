fbf919d43b6d85e1b4c686957eb4663a4c91ef52
kimi <hakimihamzan20@gmail.com>
2022-05-22 15:29:25 +0800

    Add migration for transaction table

---
+++ database/migrations/2022_05_22_045134_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//cmd used
+// php artisan make:model Transaction -m
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained();
+            $table->date('transaction_date');
+            $table->integer('amount');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\TransactionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('categories', [CategoryController::class, 'index']);
+// Route::post('categories', [CategoryController::class, 'store']);
+// Route::get('categories/{category}', [CategoryController::class, 'show']);
+// Route::put('categories/{category}', [CategoryController::class, 'update']);
+// Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+
+// Route::resource --- for full controller web
+
+Route::apiResource('categories', CategoryController::class);
+
+Route::apiResource('transactions', TransactionController::class);

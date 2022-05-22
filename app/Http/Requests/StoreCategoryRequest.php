79bed4ec3dced227269d9af45d8ae8acec000d34
kimi <hakimihamzan20@gmail.com>
2022-05-22 15:27:46 +0800

    Add extra formReq for validation

---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+// this is the cmd
+// php artisan make:request StoreCategoryRequest
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required'
+        ];
+    }
+}
